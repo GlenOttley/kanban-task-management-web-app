90a38aa8cc7467a068f3e88c28ab0782bc1d52d6
Glen Ottley <glenottley@gmail.com>
2023-07-10 14:06:13 -0400

    handle production env

--- packages/api/src/utils/server.ts
+++ packages/api/src/utils/server.ts
@@ -1,9 +1,10 @@
-import express, { Application } from 'express'
+import express, { Application, Request, Response } from 'express'
 import boardRoutes from '../routes/boardRoutes'
 import taskRoutes from '../routes/taskRoutes'
-import cors from 'cors'
+import path from 'path'
 
 function createServer() {
+  const serverRoot = path.resolve()
   const app: Application = express()
 
   // app.use(cors({ origin: 'http://localhost:3000' }))
@@ -12,6 +13,17 @@ function createServer() {
   // routes
   app.use('/api/boards', boardRoutes)
   app.use('/api/tasks', taskRoutes)
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(serverRoot, 'packages/web/dist')))
+    app.get('*', (req: Request, res: Response) =>
+      res.sendFile(path.resolve(serverRoot, 'packages', 'web', 'dist', 'index.html'))
+    )
+  } else {
+    app.get('/', (req: Request, res: Response) => {
+      res.json('API is running...')
+    })
+  }
 
   return app
 }
